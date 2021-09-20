126f248b2d24e1dd6abcb18f4992b664cfafedaa
PlutoThePlanet <pmortensen0916@gmail.com>
2021-09-19 23:29:58 -0700

    hopefully complete code

--- insult_generator.py
+++ insult_generator.py
@@ -1,31 +1,40 @@
 from random import choice
+import time
 
 #lists
-adjectives = ['wet', 'big']
-nouns = ['turnip', 'dog']
-
-#user input
-name = input('What is your name?')
-print ('Hello ' + name)
-
-age = input('How old are you?')
-if (age < 16):
-    print ('young')
-else:
-    print ('old')
-
+adjectives = ['wet', 'big', 'dumb', 'stinky', 'lazy']
+nouns = ['turnip', 'dog', 'goat', 'monke', 'booger']
 
 #functions
 def printInsult (name, age):
-    count = 0
-    if (count == 0):
-        print (name + ', you are a ' + age + ' ' + choice(adjectives) + ' ' + choice(nouns))
-        count+1
-    else:
-        userAnswer = input('can you take any more?')
-        while (userAnswer != 'no'):
-            print (name + ', you are a ' + age + ' ' + choice(adjectives) + ' ' + choice(nouns))
+    print (name + ', you are a ' + age + ' ' + choice(adjectives) + ' ' + choice(nouns))
+    
 
+#program
+name = input('What is your name?')
+if name == 'paige':
+    print('ඞ ඞ ඞ')
+    quit()
 
-#print insult(s)
-printInsult(name, age)
+age = input('How old are you?')
+if (age < 16):
+    age = 'young'
+else:
+    age = 'old'
+
+choice = input('1: print insults at your demand\n 2: print insults endlessly\n 0: exit')
+if(choice == 1):
+    printInsult(name, age)
+    cont = input('Can you handle more (y/n)')
+    while cont != 'n':
+        printInsult(name, age)
+        cont = input('Can you handle more (y/n)')
+    quit()
+elif(choice == 2):
+    while choice == 2:
+        printInsult(name, age)
+        time.sleep(3)
+elif(choice == 0):
+    quit()
+else:
+    print('not an option, try again')
